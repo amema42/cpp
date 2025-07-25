8b26a33c887d610a7287b790183f41869a909d1c
Ani Mema <amema@e3r4p3.42roma.it>
2025-07-25 11:51:00 +0200

    init cpp06/ex00

---
+++ cpp06/ex00/ScalarConverter.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string>
+
+class ScalarConverter {
+private:
+    ScalarConverter();
+    ScalarConverter(const ScalarConverter& other);
+    ScalarConverter& operator=(const ScalarConverter& other);
+    ~ScalarConverter();
+
+public:
+    static void convert(const std::string& literal);
+};
